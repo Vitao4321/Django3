58e0a684a24bde1b6c2c2994ec1afd5570b4a383
Victor Jorge Sims <victorjsims@gmail.com>
2023-10-30 21:02:20 -0300

    Initial commit

---
+++ clientes/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+    def validate_cpf(self, cpf):
+        if (len(cpf) != 11):
+            raise serializers.ValidationError("O CPF deve ter 11 digitos")
+        return cpf
+    
+
+    
